215035f9865c8048c7b782748dee1d258b2ffa5f
Yurev-Timofey <yurev.timofey@gmail.com>
2020-11-23 18:00:49 +0300

    Теперь после создания .fifo файла бот не уходит в оффлайн

--- discord_bot.py
+++ discord_bot.py
@@ -1,9 +1,9 @@
-import asyncio
 import os
 import discord
+from discord.ext import tasks
+
 
 client = discord.Client()
-
 token = open('discord_token.txt', 'r').read()
 prev_msg_id = 0
 path = "pipe.fifo"
@@ -14,27 +14,35 @@ if os.path.exists(path):
 @client.event
 async def on_ready():
     print('Logged in discord as {0.user}'.format(client))
-    client.loop.create_task(check_telegram_message())
+    check_telegram_message.start()
 
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "эй" in message.content.lower():
+        await message.channel.send('Привет-привет!')
+
+
+@tasks.loop(seconds=0.5)
 async def check_telegram_message():
     global prev_msg_id
-    while not client.is_closed():
-        if not os.path.exists(path):
-            continue
 
-        pipe = open(path, "r")
-        message = pipe.read()
+    if not os.path.exists(path):
+        return
 
-        if message != '':
-            msg_id, msg_text, channel_id = message.split('$space$')
-            if msg_id != prev_msg_id:
-                prev_msg_id = msg_id
-                print("Отправлено \"{}\" в канал \"{}\"".format(msg_text, channel_id))
-                await client.get_channel(int(channel_id)).send(msg_text)
+    pipe = os.open(path, os.O_RDONLY | os.O_NONBLOCK)
+    message = os.read(pipe, 2000).decode('utf-8')
+    os.close(pipe)
 
-        pipe.close()
-        await asyncio.sleep(0.5)
+    if message != '':
+        msg_id, msg_text, channel_id = message.split('$space$')
+        if msg_id != prev_msg_id:
+            prev_msg_id = msg_id
+            print("Отправлено \"{}\" в канал \"{}\"".format(msg_text, channel_id))
+            await client.get_channel(int(channel_id)).send(msg_text)
 
 
 client.run(token)
